8e5e75a380fea7e8660dc1c2e70b9af01c4bda9c
Shakil Ahmed Atik <shakilatik@gmail.com>
2023-01-01 23:19:35 +0600

    Custom button created

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,15 @@
+import { View, TextInput } from 'react-native'
+import React from 'react'
+import PrimaryButton from '../components/PrimaryButton'
+
+const StartGameScreen = () => {
+  return (
+    <View>
+      <TextInput />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
+    </View>
+  )
+}
+
+export default StartGameScreen
