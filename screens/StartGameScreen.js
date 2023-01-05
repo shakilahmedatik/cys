1c0ec99b4f9aa01d3952a3052e5725be8248c9ea
Shakil Ahmed Atik <shakilatik@gmail.com>
2023-01-05 22:33:48 +0600

    Linear background added

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -29,7 +29,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     marginTop: 100,
     padding: 16,
-    backgroundColor: '#4e0329',
+    backgroundColor: '#3b021f',
     marginHorizontal: 24,
     borderRadius: 8,
     elevation: 4,
