ab8e60a1790fe37c363f7435d967ba5ab6f72dfc
Shakil Ahmed Atik <shakilatik@gmail.com>
2023-01-20 07:45:08 +0600

    image added in game over com

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,11 +1,19 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Image } from 'react-native'
 import React from 'react'
 import Title from '../components/ui/Title'
+import Colors from '../constants/colors'
 
 const GameOverScreen = () => {
   return (
     <View style={styles.screen}>
-      <Title>GameOverScreen</Title>
+      <Title>Game Over!</Title>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={require('../assets/images/success.png')}
+        />
+      </View>
+      <Text>Your phone needed x rounds to guess the number y.</Text>
     </View>
   )
 }
@@ -16,6 +24,19 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 24,
     justifyContent: 'center',
-    backgroundColor: 'tomato',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderColor: Colors.primary800,
+    borderWidth: 3,
+    overflow: 'hidden',
+    margin: 36,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
   },
 })
